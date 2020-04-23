5a1f098597b82c0e198a9256a01171e2a2749ea0
Shaun Reed <shaunrd0@gmail.com>
2020-04-23 01:44:57 +0000

    Add postfix ansible roll

---
+++ ansible/roles/postfix/files/postfix/init-mail.sh
@@ -0,0 +1,3 @@
+sudo postmap /etc/postfix/sasl/sasl_passwd;
+sudo chown root:root /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db;
+sudo chmod 600 /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db;
