49eb64f320bc38252c0cd2a1d9108fc70cbf3ec7
Shaun Reed <shaunrd0@gmail.com>
2021-06-04 12:08:37 -0400

    Add examples of binary search tree algorithms + Using the pseudocode examples from MIT introduction to algorithms

---
+++ cpp/algorithms/trees/binary/bst.h
@@ -0,0 +1,84 @@
+/*#############################################################################
+## Author: Shaun Reed                                                        ##
+## Legal: All Content (c) 2021 Shaun Reed, all rights reserved               ##
+## About: An example of a binary search tree implementation                  ##
+##        The algorithms in this example are seen in MIT Intro to Algorithms ##
+##                                                                           ##
+## Contact: shaunrd0@gmail.com  | URL: www.shaunreed.com | GitHub: shaunrd0  ##
+##############################################################################
+## bst.h
+*/
+
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+
+// TODO: Add balance() method to balance overweight branches
+class BinarySearchTree {
+
+public:
+  // BinaryNode Structure
+  struct BinaryNode{
+    int element;
+    BinaryNode *left, *right, *parent;
+
+    // Ctor for specific element, lhs, rhs
+    BinaryNode(const int &el, BinaryNode *lt, BinaryNode *rt, BinaryNode *p)
+        :element(el), left(lt), right(rt), parent(p) {};
+    // Ctor for a node and any downstream nodes
+    explicit BinaryNode(BinaryNode * toCopy);
+  };
+
+  BinarySearchTree() : root(nullptr) {};
+  BinarySearchTree(const BinarySearchTree &rhs) : root(rhs.clone(rhs.root)) {};
+  BinarySearchTree& operator=(const BinarySearchTree& rhs);
+  ~BinarySearchTree() { makeEmpty(root);};
+  inline BinaryNode * getRoot() const { return root;}
+
+  // Check if value is within the tree or subtree
+  inline bool contains(const int &value) const { return contains(value, root);}
+  bool contains(const int &value, BinaryNode *start) const;
+
+  // Empties a given tree or subtree
+  inline void makeEmpty() { makeEmpty(root);}
+  void makeEmpty(BinaryNode *&tree);
+  // Checks if this BST is empty
+  bool isEmpty() const;
+
+  // Insert and remove values from a tree or subtree
+  inline void insert(const int &x) { insert(x, root, nullptr);}
+  void insert(const int &newValue, BinaryNode *&start, BinaryNode *prevNode);
+  inline void remove(const int &x) { remove(search(x, root));}
+  void remove(BinaryNode *removeNode);
+
+  // Traversal functions
+  inline void printInOrder() const { printInOrder(root);}
+  inline void printPostOrder() const { printPostOrder(root);}
+  inline void printPreOrder() const { printPreOrder(root);}
+  // Overloaded to specify traversal of a subtree
+  void printInOrder(BinaryNode *start) const;
+  void printPostOrder(BinaryNode *start) const;
+  void printPreOrder(BinaryNode *start) const;
+
+  // Find a BinaryNode containing value starting at a given tree / subtree node
+  inline BinaryNode * search(const int &value) const { return search(value, root);}
+  BinaryNode * search(const int &value, BinaryNode *start) const;
+  inline BinaryNode * findMin() const { return findMin(root);}
+  inline BinaryNode * findMax() const { return findMax(root);}
+  // Find nodes with min / max values starting at a given tree / subtree node
+  BinaryNode * findMin(BinaryNode *start) const;
+  BinaryNode * findMax(BinaryNode *start) const;
+
+  BinaryNode * predecessor(BinaryNode *startNode) const;
+  BinaryNode * successor(BinaryNode *startNode) const;
+
+private:
+  // BST Private Member Functions
+  static BinaryNode * clone(BinaryNode *start);
+  void transplant(BinaryNode *oldNode, BinaryNode *newNode);
+
+  BinaryNode *root;
+};
+
+#endif // BST_H
---
+++ cpp/algorithms/trees/binary/driver.cpp
@@ -0,0 +1,96 @@
+/*#############################################################################
+## Author: Shaun Reed                                                        ##
+## Legal: All Content (c) 2021 Shaun Reed, all rights reserved               ##
+## About: Driver program to test BST algorithms from MIT intro to algorithms ##
+##                                                                           ##
+## Contact: shaunrd0@gmail.com  | URL: www.shaunreed.com | GitHub: shaunrd0  ##
+###############################################################################
+*/
+
+#include "bst.h"
+
+#include <iostream>
+#include <vector>
+
+
+int main (const int argc, const char * argv[])
+{
+  BinarySearchTree testTree;
+  std::vector<int> inputValues {2, 6, 9, 1, 5, 8, 10};
+  // Alternative input values
+//  std::vector<int> inputValues {10, 12, 6, 4, 20, 8, 7, 15, 13};
+
+  std::cout << "Building binary search tree with input: ";
+  for (const auto &value : inputValues) std::cout << value << ", ";
+  std::cout << std::endl;
+
+  for (const auto &value : inputValues) testTree.insert(value);
+
+  std::cout << "\nInorder traversal: \n";
+  testTree.printInOrder();
+  std::cout << std::endl;
+
+  std::cout << "Postorder traversal: \n";
+  testTree.printPostOrder();
+  std::cout << std::endl;
+
+  std::cout << "Preorder traversal: \n";
+  testTree.printPreOrder();
+  std::cout << std::endl;
+
+  std::cout << "\nMinimum value: " << testTree.findMin()->element << std::endl;
+  std::cout << "Maximum value: " << testTree.findMax()->element << std::endl;
+
+  // Test removing a node, printing the result in-order
+  std::cout << "\nRemoving root value...\n";
+  testTree.remove(testTree.getRoot()->element);
+  // Can use inline function to remove a value directly for testing -
+//  testTree.remove(6);
+
+  std::cout << "Inorder traversal: \n";
+  testTree.printInOrder();
+  std::cout << std::endl;
+
+  // Test copy constructor
+  std::cout << "\nCloning testTree to testTree2...\n";
+  BinarySearchTree testTree2 = testTree;
+  std::cout << "Inorder traversal of the cloned testTree2: \n";
+  testTree2.printInOrder();
+  std::cout << std::endl;
+
+  // Test assignment operator
+  std::cout << "\nCloning testTree to testTree3...\n";
+  BinarySearchTree testTree3;
+  testTree3 = testTree;
+  std::cout << "Inorder traversal of the cloned testTree3 tree: \n";
+  testTree3.printInOrder();
+  std::cout << std::endl;
+
+  // Test emptying the BST
+  std::cout << "\nEmptying testTree...\n";
+  testTree.makeEmpty();
+  std::cout << "testTree isEmpty: " << (testTree.isEmpty() ? "true" : "false");
+  std::cout << "\ntestTree2 isEmpty: " << (testTree2.isEmpty() ? "true" : "false");
+  std::cout << "\ntestTree3 isEmpty: " << (testTree3.isEmpty() ? "true" : "false");
+
+  // Testing integrity of deep copy (cloned) data after deletion of testTree
+  std::cout << "\n\nTesting integrity of previously cloned testTree2...\n";
+  std::cout << "Inorder traversal of the cloned testTree2: \n";
+  testTree2.printInOrder();
+  std::cout << std::endl;
+  std::cout << "Inorder traversal of the cloned testTree3: \n";
+  testTree3.printInOrder();
+  std::cout << std::endl;
+
+  std::cout << "\nChecking if tree contains value of 6: ";
+  std::cout << (testTree2.contains(6) ? "true" : "false") << std::endl;
+  std::cout << "Checking if tree contains value of 600: ";
+  std::cout << (testTree2.contains(600) ? "true" : "false") << std::endl;
+
+  std::cout << "\nSuccessor of node with value 6: "
+            << testTree2.successor(testTree2.search(6))->element;
+  std::cout << "\nPredecessor of node with value 6: "
+            << testTree2.predecessor(testTree2.search(6))->element;
+  std::cout << std::endl;
+
+}
