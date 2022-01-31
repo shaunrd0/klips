bbbf404340816b13bbd5e9fc3cb1f9e89f411c09
Shaun Reed <shaunrd0@gmail.com>
2022-01-31 08:51:50 -0500

    [cmake-example] Update cmake project example
    + Rename `cmake` -> `cmake-example`

---
+++ cpp/cmake-example/apps/say-hello.cpp
@@ -0,0 +1,19 @@
+/*##############################################################################
+## Author: Shaun Reed                                                         ##
+## Legal: All Content (c) 2022 Shaun Reed, all rights reserved                ##
+##                                                                            ##
+## Contact: shaunrd0@gmail.com	| URL: www.shaunreed.com | GitHub: shaunrd0   ##
+################################################################################
+*/
+
+#include <lib-klips.hpp>
+#include <iostream>
+
+int main () {
+	PrintHello(5);
+	std::cout << "Press enter to exit the application. \n";
+	std::cin.ignore();
+	
+	return 0;
+}
+
---
+++ cpp/cmake-example/include/lib-klips.hpp
@@ -0,0 +1,14 @@
+/*##############################################################################
+## Author: Shaun Reed                                                         ##
+## Legal: All Content (c) 2022 Shaun Reed, all rights reserved                ##
+##                                                                            ##
+## Contact: shaunrd0@gmail.com	| URL: www.shaunreed.com | GitHub: shaunrd0   ##
+################################################################################
+*/
+
+/**
+ * Prints "Hello World!" \p n times to console.
+ * @param n The number of times to print "Hello World!"
+ */
+void PrintHello(int n);
+
